7ef5bb792d4dddbc9705c4baa0d133d11fd60a87
arismoko <figueredoarian@gmail.com>
2024-08-02 14:31:27 -0400

    Had gpt generate various keybinds that we will probably have to fix

--- keybinds.lua
+++ keybinds.lua
@@ -3,7 +3,9 @@ local KeyHandler = require("KeyHandler"):getInstance()
 local Model = require("Model"):getInstance()
 local View = require("View"):getInstance()
 
--- Normal mode keybindings
+-- === Normal Mode Keybindings ===
+
+-- Basic Navigation
 KeyHandler:map("normal", "h", function()
     Model.cursorX = math.max(1, Model.cursorX - 1)
 end)
@@ -26,6 +28,197 @@ KeyHandler:map("normal", "l", function()
     Model.cursorX = math.min(#Model.buffer[Model.cursorY] + 1, Model.cursorX + 1)
 end)
 
+-- Word and Line Motions
+KeyHandler:map("normal", "w", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextSpace = line:find("%s", Model.cursorX)
+    if nextSpace then
+        Model.cursorX = nextSpace + 1
+    else
+        Model.cursorX = #line + 1
+    end
+end)
+
+KeyHandler:map("normal", "shift + w", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextSpace = line:find("%s", Model.cursorX)
+    if nextSpace then
+        local nextWordStart = line:find("%S", nextSpace + 1)
+        if nextWordStart then
+            Model.cursorX = nextWordStart
+        else
+            Model.cursorX = #line + 1
+        end
+    else
+        Model.cursorX = #line + 1
+    end
+end)
+
+KeyHandler:map("normal", "b", function()
+    local line = Model.buffer[Model.cursorY]
+    local prevSpace = line:sub(1, Model.cursorX - 1):find("%s[^%s]*$")
+    if prevSpace then
+        Model.cursorX = prevSpace
+    else
+        Model.cursorX = 1
+    end
+end)
+
+KeyHandler:map("normal", "shift + b", function()
+    local line = Model.buffer[Model.cursorY]
+    local prevWordEnd = line:sub(1, Model.cursorX - 1):find("%S[%s]*$")
+    if prevWordEnd then
+        local prevWordStart = line:sub(1, prevWordEnd):find("%s*[^%s]*$")
+        Model.cursorX = prevWordStart
+    else
+        Model.cursorX = 1
+    end
+end)
+
+KeyHandler:map("normal", "e", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextWordEnd = line:find("[^%s]+", Model.cursorX)
+    if nextWordEnd then
+        Model.cursorX = nextWordEnd + line:sub(nextWordEnd):find("%s") - 1
+    else
+        Model.cursorX = #line + 1
+    end
+end)
+
+KeyHandler:map("normal", "shift + e", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextWordEnd = line:find("%S[%s]*", Model.cursorX)
+    if nextWordEnd then
+        Model.cursorX = nextWordEnd
+    else
+        Model.cursorX = #line + 1
+    end
+end)
+
+KeyHandler:map("normal", "0", function()
+    Model.cursorX = 1
+end)
+
+KeyHandler:map("normal", "shift + 4", function()
+    Model.cursorX = #Model.buffer[Model.cursorY] + 1
+end)
+
+KeyHandler:map("normal", "shift + 6", function()
+    local line = Model.buffer[Model.cursorY]
+    local firstNonBlank = line:find("%S")
+    if firstNonBlank then
+        Model.cursorX = firstNonBlank
+    else
+        Model.cursorX = 1
+    end
+end)
+
+-- File and Screen Navigation
+KeyHandler:map("normal", "g", function()
+    local pressedG = false
+    return function(key)
+        if key == keys.g then
+            if pressedG then
+                Model.cursorY = 1
+                Model.cursorX = 1
+                pressedG = false
+                View:drawScreen()
+                Model:updateStatusBar("Moved to top of file")
+            else
+                pressedG = true
+            end
+        else
+            pressedG = false
+        end
+    end
+end)
+
+KeyHandler:map("normal", "shift + g", function()
+    Model.cursorY = #Model.buffer
+    Model.cursorX = 1
+end)
+
+KeyHandler:map("normal", "shift + h", function()
+    local screenStart = Model.scrollOffset + 1
+    Model.cursorY = screenStart
+    Model.cursorX = 1
+    View:drawScreen()
+    Model:updateStatusBar("Moved to top of screen")
+end)
+
+KeyHandler:map("normal", "shift + m", function()
+    local screenHeight = View:getScreenHeight()
+    local screenMiddle = math.floor(screenHeight / 2)
+    Model.cursorY = Model.scrollOffset + screenMiddle
+    Model.cursorX = 1
+    View:drawScreen()
+    Model:updateStatusBar("Moved to middle of screen")
+end)
+
+KeyHandler:map("normal", "shift + l", function()
+    local screenHeight = View:getScreenHeight()
+    local screenEnd = Model.scrollOffset + screenHeight - 1
+    Model.cursorY = screenEnd
+    Model.cursorX = 1
+    View:drawScreen()
+    Model:updateStatusBar("Moved to bottom of screen")
+end)
+
+-- Editing
+KeyHandler:map("normal", "x", function()
+    local line = Model.buffer[Model.cursorY]
+    if Model.cursorX <= #line then
+        Model.buffer[Model.cursorY] = line:sub(1, Model.cursorX - 1) .. line:sub(Model.cursorX + 1)
+        View:drawLine(Model.cursorY - Model.scrollOffset)
+        Model:updateStatusBar("Deleted character")
+    else
+        Model:updateStatusError("Nothing to delete")
+    end
+end)
+
+KeyHandler:map("normal", "shift + x", function()
+    local line = Model.buffer[Model.cursorY]
+    if Model.cursorX > 1 then
+        Model.buffer[Model.cursorY] = line:sub(1, Model.cursorX - 2) .. line:sub(Model.cursorX)
+        Model.cursorX = Model.cursorX - 1
+        View:drawLine(Model.cursorY - Model.scrollOffset)
+        Model:updateStatusBar("Deleted character")
+    else
+        Model:updateStatusError("Nothing to delete")
+    end
+end)
+
+KeyHandler:map("normal", "d", function()
+    Model:cutLine()
+    View:drawScreen()
+end)
+
+KeyHandler:map("normal", "d + w", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextSpace = line:find("%s", Model.cursorX)
+    if nextSpace then
+        line = line:sub(1, Model.cursorX - 1) .. line:sub(nextSpace + 1)
+    else
+        line = line:sub(1, Model.cursorX - 1)
+    end
+    Model.buffer[Model.cursorY] = line
+    Model:updateStatusBar("Deleted word")
+    View:drawScreen()
+end)
+
+KeyHandler:map("normal", "c + w", function()
+    local line = Model.buffer[Model.cursorY]
+    local nextSpace = line:find("%s", Model.cursorX)
+    if nextSpace then
+        line = line:sub(1, Model.cursorX - 1) .. line:sub(nextSpace + 1)
+    else
+        line = line:sub(1, Model.cursorX - 1)
+    end
+    Model.buffer[Model.cursorY] = line
+    Model:switchMode("insert")
+    View:drawScreen()
+end)
+
 KeyHandler:map("normal", "y", function()
     Model:yankLine()
 end)
@@ -34,11 +227,6 @@ KeyHandler:map("normal", "p", function()
     Model:paste()
     View:drawLine(Model.cursorY - Model.scrollOffset)
     Model:updateStatusBar("Pasted text")
-end)
-
-KeyHandler:map("normal", "d", function()
-    Model:cutLine()
-    View:drawScreen()
 end)
 
 KeyHandler:map("normal", "u", function()
@@ -51,21 +239,85 @@ KeyHandler:map("normal", "ctrl + u", function()
     View:drawScreen()
 end)
 
+-- Mode Switching
+KeyHandler:map("normal", "i", function()
+    Model:switchMode("insert")
+end)
+
+KeyHandler:map("normal", "a", function()
+    Model.cursorX = math.min(Model.cursorX + 1, #Model.buffer[Model.cursorY] + 1)
+    Model:switchMode("insert")
+end)
+
+KeyHandler:map("normal", "shift + a", function()
+    Model.cursorX = #Model.buffer[Model.cursorY] + 1
+    Model:switchMode("insert")
+end)
+
+KeyHandler:map("normal", "shift + i", function()
+    local line = Model.buffer[Model.cursorY]
+    local firstNonBlank = line:find("%S")
+    if firstNonBlank then
+        Model.cursorX = firstNonBlank
+    else
+        Model.cursorX = 1
+    end
+    Model:switchMode("insert")
+end)
+
+KeyHandler:map("normal", "o", function()
+    local line = Model.cursorY
+    table.insert(Model.buffer, line + 1, "")
+    Model.cursorY = line + 1
+    Model.cursorX = 1
+    Model:switchMode("insert")
+    View:drawScreen()
+end)
+
+KeyHandler:map("normal", "shift + o", function()
+    local line = Model.cursorY
+    table.insert(Model.buffer, line, "")
+    Model.cursorY = line
+    Model.cursorX = 1
+    Model:switchMode("insert")
+    View:drawScreen()
+end)
+
 KeyHandler:map("normal", "shift + semicolon", function()
     Model:switchMode("command")
 end)
-KeyHandler:map("normal", "i", function ()
-    Model:switchMode("insert")
-end)
-    
+
 KeyHandler:map("normal", "v", function()
     Model:startVisualMode()
 end)
+
 KeyHandler:map("normal", "f9", function()
     Model.shouldExit = true
 end)
 
--- Visual mode keybindings
+-- Miscellaneous
+KeyHandler:map("normal", "f1", function()
+    local keybindsWindow = View:createWindow(1, 1)
+    keybindsWindow:print("Current Keybindings:")
+    for mode, keyMap in pairs(KeyHandler.keyMap) do
+        keybindsWindow:writeline(" ")
+        keybindsWindow:writeline("Mode: " .. mode)
+        local i = 1
+        for key, _ in pairs(keyMap) do
+            keybindsWindow:write("  " .. i .. ": " .. key .. ",")
+            i = i + 1
+        end
+    end
+    keybindsWindow:show()
+end)
+
+KeyHandler:map("normal", "f4", function()
+    View:closeAllWindows()
+end)
+
+-- === Visual Mode Keybindings ===
+
+-- Basic Navigation
 KeyHandler:map("visual", "h", function()
     Model.cursorX = math.max(1, Model.cursorX - 1)
     View:drawScreen()
@@ -92,6 +344,7 @@ KeyHandler:map("visual", "l", function()
     View:drawScreen()
 end)
 
+-- Editing
 KeyHandler:map("visual", "y", function()
     Model:yankSelection()
     Model:endVisualMode()
@@ -102,33 +355,14 @@ KeyHandler:map("visual", "d", function()
     View:drawScreen()
 end)
 
-KeyHandler:map("visual", "escape", "switch:normal")
-
--- Insert mode keybindings
-KeyHandler:map("insert", "escape", "switch:normal")
-
-KeyHandler:map("normal", "f1", function()
-    -- Create a new window that uses the maximum available size
-    local keybindsWindow = View:createWindow(1, 1)
-
-    -- Print some text in the window
-    keybindsWindow:print("Current Keybindings:")
-    for mode, keyMap in pairs(KeyHandler.keyMap) do
-        keybindsWindow:writeline(" ")
-        keybindsWindow:writeline("Mode: " .. mode)
-        local i = 1
-        for key, _ in pairs(keyMap) do
-            keybindsWindow:write("  " .. i .. ": " .. key .. ",")
-            i = i + 1
-        end
-    end
-
-    -- Show the window
-    keybindsWindow:show()
+-- Mode Switching
+KeyHandler:map("visual", "escape", function()
+    Model:endVisualMode()
 end)
 
-KeyHandler:map("normal", "f4", function()
-    View:closeAllWindows()
+-- === Insert Mode Keybindings ===
+
+-- Mode Switching
+KeyHandler:map("insert", "escape", function()
+    Model:switchMode("normal")
 end)
-
-
