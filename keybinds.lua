a0a93b40416dd90e2310c04f8685351a24e77712
arismoko <figueredoarian@gmail.com>
2024-08-02 12:26:22 -0400

    fixed debug keybinds

--- keybinds.lua
+++ keybinds.lua
@@ -117,7 +117,7 @@ KeyHandler:map("normal", keys.f1, function()
     keybindsWindow:show()
 end)
 
-KeyHandler:map("normal", keys.f1, function()
+KeyHandler:map("normal", keys.f4, function()
     View:closeAllWindows()
 end)
 
